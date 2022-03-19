5e23b65402e54200e7219634dd28a9dab5315400
Marek Hromádko <marek.hromadko@educhem.cz>
2022-03-19 15:28:08 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadej 1. číslo");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("zadej 2. číslo");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine(x);
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine(y);
+            }
+            else
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Obě čísla jsou stejně velká");
+            }
+        }
+    }
+}
